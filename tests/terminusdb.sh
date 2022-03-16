238d6ce9afb8b6d1708b1d063350443e420c87b0
Sean Leather <sean@terminusdb.com>
2022-03-16 14:24:18 +0200

    comments, set -ex

--- tests/terminusdb.sh
+++ tests/terminusdb.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
+# This is a script used to test the command-line interface (CLI).
+# It should be run in this directory.
+
+# 1. Try to use the locally built executable. This is for development.
+# 2. Try to use the Docker image. This is for continuous integration.
+
 if [[ -x "../terminusdb" ]]; then
+  set -ex
   ../terminusdb "$@"
 elif docker image inspect terminusdb/terminusdb-server:local > /dev/null; then
+  user="$(id -u):$(id -g)"
+  set -ex
   docker run \
     --rm \
-    --user $(id -u):$(id -g) \
-    --volume $(pwd):/app/terminusdb/tests \
+    --user $user \
+    --volume $PWD:/app/terminusdb/tests \
     --workdir /app/terminusdb/tests \
     terminusdb/terminusdb-server:local \
     /app/terminusdb/terminusdb \
     "$@"
 else
-  echo "Error! No way found to run terminusdb."
+  echo "Error! I'm not sure how to run the CLI (terminusdb)."
   exit -1
 fi
